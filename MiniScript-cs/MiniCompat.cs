9cf40e08900a71bb4ab57ad2ccc8f2413b174959
Bananahemic <bananahemic@users.noreply.github.com>
2020-02-14 01:59:58 -0800

    Added compat to log on all platforms

---
+++ MiniScript-cs/MiniCompat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class MiniCompat
+{
+    public static void LogWarning(string wrn)
+    {
+#if UNITY_5_3_OR_NEWER
+        Debug.LogWarning(wrn);
+#else
+        Console.WriteLine("WRN " + wrn);
+#endif
+    }
+    public static void LogError(string err)
+    {
+#if UNITY_5_3_OR_NEWER
+        Debug.LogError(err);
+#else
+        Console.WriteLine("ERR " + wrn);
+#endif
+    }
+}
